cca50854f27a34b0dac704f167d48e59ee9694d7
KhushiMK <100280301+KhushiMK@users.noreply.github.com>
2024-01-28 11:28:48 +0530

    Create Helloworld.java

---
+++ Lecture1(Datatypes and Variable)/Helloworld.java
@@ -0,0 +1,7 @@
+public class Helloworld
+{
+    public static void main(String[] args) {
+        System.out.println("Hello World");//this is statement terminator
+    }
+}
+/*  print and println is a function to display the content between the paranthesis  the 'S' of System should be uppercase and 'O' of out should be lowercase*/
