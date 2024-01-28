cab0d91218b18d9d81f1e6a7d11e9571f242bcb4
KhushiMK <100280301+KhushiMK@users.noreply.github.com>
2024-01-28 11:45:04 +0530

    Update Helloworld.java

--- Lecture1(Datatypes and Variable)/Helloworld.java
+++ Lecture1(Datatypes and Variable)/Helloworld.java
@@ -1,7 +1,8 @@
-public class Helloworld
+public class Helloworld//the name of java class and file name should be same
 {
     public static void main(String[] args) {
         System.out.print("Hello World");//this is statement terminator
     }
 }
-/*  print  is a function to display the content between the parenthesis  the 'S' of System should be uppercase and the 'O' of out should be lowercase*/
+/*  print  is a function to display the content between the parenthesis  the 'S' of System should be uppercase and the 'O' of out should be lowercase 
+boilerplate code (template code)*/
