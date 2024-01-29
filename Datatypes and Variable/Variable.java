5c32d47f4bb9afebf5577c5c55a4ccb29688e35c
KhushiMK <100280301+KhushiMK@users.noreply.github.com>
2024-01-29 22:14:34 +0530

    Update and rename pattern.java to Variable.java

---
+++ Datatypes and Variable/Variable.java
@@ -0,0 +1,19 @@
+public class Variable
+{
+    public static void main(String args[]) 
+    {
+       int a= 10;
+       int b = 20;
+       System.out.println(a);
+       System.out.println(b);
+       String name = "KhushiMK";
+       System.out.println(name);
+       a= 50;// this is how anyone can variable value
+       System.out.println(a);
+       a=b;//two variables can have the same value
+       System.out.println(a);
+    }
+}
+/* The println is a function with a line gap between the present link and the next line. 
+'a', and,'b' are the variable
+*/
