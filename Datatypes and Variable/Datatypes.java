bad95bba685d7f1c8be6d941e2aeed9b012d875c
KhushiMK <100280301+KhushiMK@users.noreply.github.com>
2024-01-30 15:44:37 +0530

    Create Datatypes.java

---
+++ Datatypes and Variable/Datatypes.java
@@ -0,0 +1,24 @@
+public class Datatypes {
+   public static void main(String[] args) {
+    byte b = 8;//it can only store value from -128 to 127 
+    System.out.println(b);
+    char ch = 'a';//it will store single character only
+    System.out.println(ch);
+    boolean var = true;// it will only store true or false
+    System.out.println(var);
+    double price =10.5;// it will store  big decimal value
+    System.out.println(price);
+    int no = 25;
+    System.out.println(no);
+    // long for big integer
+    //float for small decimal value
+    short n =240;
+    System.out.println(n);
+} 
+}
+/* 
+There are two types : Primitive and Non-Primitive
+1. Primitive : byte , short , char , boolean , int , long , float , double 
+2. Non-Primitive : String , Array , Class , Object , Interface
+-> Java is type lanuage meaning we need to specify the what type of data will be stored in the variable
+*/
