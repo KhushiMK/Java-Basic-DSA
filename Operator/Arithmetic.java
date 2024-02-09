5cbbf78576f18aa394de7d15f05dff535d2f9ca9
KhushiMK <100280301+KhushiMK@users.noreply.github.com>
2024-02-09 18:38:10 +0530

    Create Arithmetic.java

---
+++ Operator/Arithmetic.java
@@ -0,0 +1,18 @@
+public class Arithmetic {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 3;
+        System.out.println(a +" + "+ b +" = "+(a+b));
+        System.out.println( a +" - "+ b +" = "+(a-b));
+        System.out.println( a +" * "+ b +" = "+(a*b));
+        System.out.println(a +" / "+ b +" = "+(a/b));
+        System.out.println(a +" % "+ b +" = "+(a%b));
+    }
+}
+/* Types of Arithmetic Operator
+-> Binary : + , - , * , / , %
+->Unary : ++(increment) , --(decrement)
+->Pre Increment /decrement: 1 Value Change  2 value Use
+->post Increment /decrement : 1 Value Use 2 Value Change  
+
+*/
