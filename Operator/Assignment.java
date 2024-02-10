2b1dcc357f37ed5ff0391158c9b842f14cd7a0e7
KhushiMK <100280301+KhushiMK@users.noreply.github.com>
2024-02-10 15:19:09 +0530

    Add files via upload

---
+++ Operator/Assignment.java
@@ -0,0 +1,23 @@
+public class Assignment {
+    public static void main(String[] args) {
+        int a = 10;
+        a+=10;
+        System.out.println(a);
+        a-=5;
+        System.out.println(a);
+        a*=5;
+        System.out.println(a);
+        a/=5;
+        System.out.println(a);
+        a%=5;
+        System.out.println(a);
+    }
+}
+/* Assignment Operator
+-> equal to (=)
+-> a = a+10 (a+= 10)
+-> a = a-4 (a-= 4)
+-> a= a*3 (a*=3)
+->a = a/5 (a/=5)
+->a = a%3(a)
+*/
