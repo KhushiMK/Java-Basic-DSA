a94feb640f19bc2e2563b3c879bc320cb0b25452
KhushiMK <100280301+KhushiMK@users.noreply.github.com>
2024-02-11 14:19:44 +0530

    Add files via upload

---
+++ Operator/Logical.java
@@ -0,0 +1,12 @@
+public class Logical {
+    public static void main(String[] args) {
+        System.out.println((3>2) && (5>0));//true case
+        System.out.println((3<2) || (5<0));//false case
+        System.out.println(!(3<2));
+    }
+}
+/* Logical Operator
+-> And Operator (&&)
+-> Or Operator(||)
+-> Not Operator(!)
+*/
