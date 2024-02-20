b14cd2a659745fc71841c71d760a297548f4571d
KhushiMK <100280301+KhushiMK@users.noreply.github.com>
2024-02-20 22:33:09 +0530

    Add files via upload

---
+++ Conditional Statement/Ternary.java
@@ -0,0 +1,7 @@
+public class Ternary {
+    public static void main(String[] args) {
+        int num = 4;
+        String type =((num%2) ==0)? "Even":"Odd";
+        System.out.println(type);
+    }
+}
