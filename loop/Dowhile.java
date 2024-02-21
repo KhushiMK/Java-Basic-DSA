cf42b954226203ef76028075e17f279e326d4759
KhushiMK <100280301+KhushiMK@users.noreply.github.com>
2024-02-21 21:06:54 +0530

    Create Dowhile.java

---
+++ loop/Dowhile.java
@@ -0,0 +1,9 @@
+public class Dowhile {
+    public static void main(String[] args) {
+        int cnt = 1;
+        do {
+            System.out.println("Hello World");
+            cnt++;
+        } while (cnt<=10);
+    }
+}
