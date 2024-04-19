a4b2283fad3519db03ed1620e3cf89ff44c8dda5
KhushiMK <100280301+KhushiMK@users.noreply.github.com>
2024-04-19 09:03:03 +0530

    Create compare.java

---
+++ String/compare.java
@@ -0,0 +1,15 @@
+public class compare {
+    public static void main(String args[])
+    {
+        String s1="Khushi";
+        String s2 = new String("Khushi");
+        if(s1.equals(s2))
+        {
+            System.out.println("String are Equal");
+        }
+        else
+        {
+            System.out.println("String are not Equal");
+        }
+    }
+}
