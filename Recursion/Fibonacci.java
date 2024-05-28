e0b699c4d4d11f95b44179bdaa6457f65907f6a3
KhushiMK <100280301+KhushiMK@users.noreply.github.com>
2024-05-28 17:18:30 +0530

    Add files via upload

---
+++ Recursion/Fibonacci.java
@@ -0,0 +1,16 @@
+/* Print nth fibonacci*/
+public class Fibonacci {
+    public static int fib(int n)
+    {
+        if(n==0 || n==1)
+        {
+            return n;
+        }
+        int f = fib(n-1) + fib(n-2);
+        return f;
+    }
+    public static void main(String[] args) {
+        int n= 10;
+        System.out.println(fib(n));
+    }
+}
